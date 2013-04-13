b03ff26d316e8ec2584c0d78935a7edc8137baa3
jamesstout <stoutyhk@gmail.com>
2013-04-13 21:35:26 +0800

    Init commit

---
+++ Cheaters/README.md
@@ -0,0 +1,7 @@
+#Add Date To Filename
+
+Select a file in Finder and hit your hotkey.
+
+Adds the *creation* date to the file before the extension:
+
+E.g. Item Name.xxx -> Item Name-20130523.xxx
--- utils/utils.sh
+++ utils/utils.sh
@@ -37,5 +37,75 @@ alf_error() {
 # get present working dir
 PWD=`pwd`
 
-# get name of script
-me=`basename $0`
+alf_is_git_repo() {
+    $(git rev-parse --is-inside-work-tree &> /dev/null)
+}
+
+# Test whether a command exists
+alf_type_exists() {
+    if [ $(type -P $1) ]; then
+      return 0
+    fi
+    return 1
+}
+
+# Git status information
+alf_git_status() {
+    local git_state uc us ut st
+	git update-index --really-refresh  -q &>/dev/null
+
+    # Check for uncommitted changes in the index
+    if ! $(git diff --quiet --ignore-submodules --cached); then
+	uc="+"
+    fi
+
+    # Check for unstaged changes
+    if ! $(git diff-files --quiet --ignore-submodules --); then
+	us="!"
+    fi
+
+    # Check for untracked files
+    if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+	ut="?"
+    fi
+
+    # Check for stashed files
+    if $(git rev-parse --verify refs/stash &>/dev/null); then
+	st="$"
+    fi
+
+    git_state=$uc$us$ut$st
+
+    echo $git_state
+
+}
+
+alf_git_overwrite() {
+
+user_response=$(osascript <<EOF
+tell application "System Events"
+	activate
+	set userCanceled to false
+	try
+		set alertResult to display alert "Your git repo has local changes, do you want to over-write them?" ¬
+			message "Your local changes will be LOST if you click YES" ¬
+		buttons {"No", "Yes"} as warning ¬
+		default button "No" cancel button "No" giving up after 5
+	on error number -128
+	set userCanceled to true
+	end try
+
+	if userCanceled then
+	    set alertResult to "NO"
+	else if gave up of alertResult then
+	    set alertResult to "NO"
+	else if button returned of alertResult is "Yes" then
+		set alertResult to "YES"
+	end if
+end tell
+
+EOF)
+
+echo "$user_response"
+
+}
